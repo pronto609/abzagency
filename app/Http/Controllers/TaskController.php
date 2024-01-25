8320546a6653ebdfdf3732c19f0ca23a11ced26c
Danaiel Brazhnyk <d.bragnik@sam-solutions.biz>
2024-01-25 21:46:56 +0200

    create api methods 1

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreTaskRequest;
+use App\Http\Resources\TaskCollection;
+use App\Http\Resources\TaskResource;
 use App\Models\Task;
 use Illuminate\Http\Request;
 
@@ -9,6 +12,19 @@ class TaskController extends Controller
 {
     public function index(Request $request)
     {
-        return response()->json(Task::all());
+        return new TaskCollection(Task::all());
+    }
+
+    public function show(Request $request, Task $task)
+    {
+        return new TaskResource($task);
+    }
+
+    public function store(StoreTaskRequest $request, Task $task)
+    {
+        $validated = $request->validated();
+
+        $task = Task::create($validated);
+        return new TaskResource($task);
     }
 }
