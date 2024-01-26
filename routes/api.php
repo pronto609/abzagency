4140e654f07c36ad4a61d1ee7a4edd84308c3062
Danaiel Brazhnyk <d.bragnik@sam-solutions.biz>
2024-01-26 10:24:52 +0200

    add authentication

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "api" middleware group. Make something great!
 |
 */
+Route::post('login', [\App\Http\Controllers\AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
